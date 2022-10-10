cd99cecb9b72ee395f4f3797ccab2a3f220dd050
Omar Bassatni <omarbassatni@gmail.com>
2022-10-10 15:50:36 +0300

    changed app title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ChooseMoviePage from './pages/ChooseMoviePage'
 import { MovieProvider } from "./store/MoviesStore";
+import { Helmet } from "react-helmet";
 
 
 function App() {
   return (
     <MovieProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Shopify Challenge</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Shopify Challenge" />
+      </Helmet>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/choose-movie" element={<ChooseMoviePage />} />
