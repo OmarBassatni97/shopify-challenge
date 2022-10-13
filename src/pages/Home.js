9d11ead7f70f11d1a5c8a5aecc02e60a80b2dc30
Omar Bassatni <omarbassatni@gmail.com>
2022-10-13 16:31:10 +0300

    added scroll to top button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieSearchBar from '../components/MovieSearchBar'
 import MovieList from '../components/MovieList'
 import NominatedList from '../components/NominatedList'
+import { AiOutlineArrowUp } from 'react-icons/ai'
 const Home = () => {
+  const [showButton, setShowButton] = useState()
+
+  useEffect(() => {
+    if (window.scrollY > 300) {
+      setShowButton(true)
+    }
+    else {
+      setShowButton(false)
+    }
+  }, [])
+  const goUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // for smoothly scrolling
+    });
+  }
   return (
     <div>
-        <MovieSearchBar />
-        <div className="md:flex">
-          <MovieList />
-          <NominatedList />
-        </div>
+      <MovieSearchBar />
+      <div className="md:flex">
+        <MovieList />
+        <NominatedList />
       </div>
+      <div className='flex justify-end'>
+        {showButton &&
+          <button onClick={goUp} className='p-3 border-2 border-solid rounded bg-slate-400 m-10 '><AiOutlineArrowUp /></button>}
+      </div>
+    </div>
   )
 }
 
